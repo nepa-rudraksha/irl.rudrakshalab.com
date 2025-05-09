252228087dd5f26b989e4558c5f17dc6cf12be5c
Nepa Rudraksha <shopifydev.nepa@gmail.com>
2025-05-09 14:56:22 +0545

    pdf debug

---
+++ database/migrations/orderid_nullable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OrderidNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+                Schema::table('irl_reports', function (Blueprint $table) {
+            $table->unsignedBigInteger('order_id')->nullable()->after('user_id');
+            // If you want to add a foreign key constraint to the orders table, uncomment below
+            // $table->foreign('order_id')->references('id')->on('orders')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+                Schema::table('irl_reports', function (Blueprint $table) {
+            // If foreign key is added, drop it first
+            // $table->dropForeign(['order_id']);
+            $table->dropColumn('order_id');
+        });
+    }
+}
