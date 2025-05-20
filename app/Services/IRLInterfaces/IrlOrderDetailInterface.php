7b2fca373ac9169b7ee6ea9f21bcb610e674b335
Arbin Maharjan <arbinmain@gmail.com>
2025-05-20 17:46:27 +0545

    Revert "Make separate service of irl detail and pdf store"
    This reverts commit c87e7695c6e7ba6dbe041a1597cfef3cdbd98591.

--- app/Services/IRLInterfaces/IrlOrderDetailInterface.php
+++ app/Services/IRLInterfaces/IrlOrderDetailInterface.php
@@ -6,9 +6,12 @@ use Illuminate\Http\UploadedFile;
 interface IrlOrderDetailInterface{
 
     public function saveOrderDetail($reference_no);
+
     public function getReferenceNo();
+    public function savePDF(string $referenceNo, string $skuNo, UploadedFile $pdf,string $order_id);
     public function deselectOrderDetail($request);
-    public function getSkuNo();
+    public function savePDFTemp($order_no,$pdf);
+    public function getOrderId();
 }
 
 ?>
